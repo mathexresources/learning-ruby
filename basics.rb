a8da7c1a03fa57e4a90941e17bb13ecb9184845f
MatheX <46656456+mathexresources@users.noreply.github.com>
2023-01-09 14:12:00 +0100

    added total basics

---
+++ basics.rb
@@ -0,0 +1,9 @@
+variable = "this is variable"
+CONSTANTE = "this is constante"
+$globalvar = "this is global variable"
+
+puts "today i learn that #{variable}, #{CONSTANTE} and #{$globalvar}"
+
+3.times do |i|
+    puts i
+end
